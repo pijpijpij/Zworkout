bc7a20ff40c9c88d3eaece87e8ac1b4c41d02e98
pijpijpij <atlassian@champault.co.uk>
2018-04-01 10:20:50 +0100

    Added missing test on SaveFeature.

--- app/src/test/java/com/pij/zworkout/workout/feature/SaveFeatureTest.kt
+++ app/src/test/java/com/pij/zworkout/workout/feature/SaveFeatureTest.kt
@@ -1,8 +1,23 @@
+/*
+ * Copyright (c) 2018, Chiswick Forest
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package com.pij.zworkout.workout.feature
 
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.anyOrNull
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import com.pij.horrocks.StateProvider
 import com.pij.utils.SysoutLogger
 import com.pij.zworkout.list.WorkoutInfo
@@ -60,6 +75,17 @@ class SaveFeatureTest {
         states.assertValue(true)
     }
 
+
+    @Test
+    fun `SaveFeature delegates to storage`() {
+        // given
+
+        // when
+        runSutOn(defaultState).test()
+
+        // then
+        verify(storageMock).save(defaultState.workout, defaultState.file)
+    }
 
     @Test
     fun `When storage succeeds, sut emits not in progress`() {
